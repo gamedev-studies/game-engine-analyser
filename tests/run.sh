5363a908628572839bda30a95ae9ffa85e209e87
Gabriel Ullmann <ullmanng162@gmail.com>
2023-04-01 12:07:52 -0400

    add test scripts

--- tests/run.sh
+++ tests/run.sh
@@ -10,3 +10,53 @@ cp $base/tests/testproject-tags.csv $base/5_moose_model_gen/inputs
 
 cd $base/5_moose_model_gen
 python3 main.py testproject
+
+# cannot test .dot file, row order changes
+# cannot test .xml file, row order changes
+# cannot test report, obviously the date/time changes
+
+# file descriptions
+description=("List of unresolved includes" "Model generation code")
+
+# generated files
+generated=("$base/4_include_graph_gen/outputs/testproject-includes-unr.csv" "$base/5_moose_model_gen/outputs/testproject-model-gen.st")
+
+# expected files
+expected=("$base/tests/expected/testproject-includes-unr.csv"  "$base/tests/expected/testproject-model-gen.st")
+
+# Iterate through the arrays
+for i in "${!generated[@]}"; do
+  # Compute the MD5 hashes
+  hash1=$(md5sum "${generated[i]}" | cut -d ' ' -f 1)
+  hash2=$(md5sum "${expected[i]}" | cut -d ' ' -f 1)
+
+  # Compare the hashes
+  if [ "$hash1" == "$hash2" ]; then
+    echo "PASSED: ${description[i]}"
+  else
+    echo "FAILED: ${description[i]}"
+  fi
+done
+
+# file descriptions
+description=("Include count (.dot)" "Include count (.xml)")
+
+# generated files
+generated=("$base/4_include_graph_gen/outputs/testproject-includes.dot" "$base/5_moose_model_gen/outputs/testproject-includes.xml")
+
+# expected files
+expected=("$base/tests/expected/testproject-includes.dot" "$base/tests/expected/testproject-includes.xml")
+
+# Iterate through the arrays
+for i in "${!generated[@]}"; do
+  # Count the number of lines
+  lines1=$(wc -l "${generated[i]}" | cut -d ' ' -f 1)
+  lines2=$(wc -l "${expected[i]}" | cut -d ' ' -f 1)
+
+  # Compare the number of lines
+  if [ "$lines1" -eq "$lines2" ]; then
+    echo "PASSED: ${description[i]}"
+  else
+    echo "FAILED: ${description[i]}"
+  fi
+done
