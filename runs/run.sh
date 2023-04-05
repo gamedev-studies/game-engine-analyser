94ed4dff6357c7f5a21b8994500b2d8a85965fb1
Gabriel Ullmann <ullmanng162@gmail.com>
2023-04-05 16:51:37 -0400

    add generic script to run any project

--- runs/run.sh
+++ runs/run.sh
@@ -1,4 +1,4 @@
-echo "Run - godot"
+echo "Run - any"
 echo "===================="
 
 # /media/ullmann/HDDGabriel/research/engines
@@ -6,12 +6,12 @@ base_tool=/home/ullmann/Documents/research/game-engine-analyser
 base_project=/home/ullmann/Documents/research/game-engines
 
 cd $base_tool/4_include_graph_gen
-sh cpp-walker.sh $base_project/godot $base_project/godot godot
+sh cpp-walker.sh $base_project/$1 $base_project/$1 $1
 
 # copy from 4 to 5
 # copy from 3 to 4
-cp ./outputs/godot-includes.dot $base_tool/5_moose_model_gen/inputs
-cp $base_tool/3_subsystem_detection/godot.csv $base_tool/5_moose_model_gen/inputs/godot-tags.csv
+cp ./outputs/$1-includes.dot $base_tool/5_moose_model_gen/inputs
+cp $base_tool/3_subsystem_detection/$1.csv $base_tool/5_moose_model_gen/inputs/$1-tags.csv
 
 cd $base_tool/5_moose_model_gen
-python3 main.py godot
+python3 main.py $1
