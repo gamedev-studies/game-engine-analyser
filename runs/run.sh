95f20892aa5c688f222efdc0f51ccdcbe6aaccb4
Gabriel Ullmann <ullmanng162@gmail.com>
2023-04-06 17:15:06 -0400

    remove hardcoded paths from main script

--- runs/run.sh
+++ runs/run.sh
@@ -1,17 +1,15 @@
 echo "Run - any"
 echo "===================="
 
-# /media/ullmann/HDDGabriel/research/engines
-base_tool=/home/ullmann/Documents/research/game-engine-analyser
-base_project=/home/ullmann/Documents/research/game-engines
+# project name, project path, tool path
 
-cd $base_tool/4_include_graph_gen
-sh cpp-walker.sh $base_project/$1 $base_project/$1 $1
+cd $3/4_include_graph_gen
+sh cpp-walker.sh $2/$1 $2/$1 $1
 
 # copy from 4 to 5
 # copy from 3 to 4
-cp ./outputs/$1-includes.dot $base_tool/5_moose_model_gen/inputs
-cp $base_tool/3_subsystem_detection/$1.csv $base_tool/5_moose_model_gen/inputs/$1-tags.csv
+cp ./outputs/$1-includes.dot $3/5_moose_model_gen/inputs
+cp $3/3_subsystem_detection/$1.csv $3/5_moose_model_gen/inputs/$1-tags.csv
 
-cd $base_tool/5_moose_model_gen
+cd $3/5_moose_model_gen
 python3 main.py $1
