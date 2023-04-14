1f4af1f2eb89d3cdd00f8e48e4c6055bec98e113
Gabriel Ullmann <ullmanng162@gmail.com>
2023-04-14 10:43:58 -0400

    improve heatmap

--- 6_architectural_map_gen/gen_heatmap.py
+++ 6_architectural_map_gen/gen_heatmap.py
@@ -2,38 +2,62 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+
 def main():
     map_dependencies = {}
-    engines = ['cocos2d-x', 'godot', 'urho3d']
-    subsystems = ['AUD','COR','DEB','EDI','FES','GMP','HID','LLR','OMP','PHY','PLA','RES','SDK','SGC','SKA','VFX']
+    engines = [ "cocos2dx", "godot", "urho3d", "flaxengine", "gameplay", "o3de", "olc", "panda3d", "piccolo" ]
+    subsystems = [ "AUD", "COR", "DEB", "EDI", "FES", "GMP", "HID", "LLR", "NIL", "OMP", "PHY", "PLA", "RES", "SDK", "SGC", "SKA", "VFX" ]
 
     for engine in engines:
-        ds = pd.read_csv('matrix_' + engine + '.csv', sep=',')
-        ds2 = ds.reindex(sorted(ds.columns), axis=1)
-        ds2 = ds2.sort_values(by=['asubsystem'])
-        map_dependencies[engine] = np.array(ds2.values[0:99, 1:17])
+        ds = pd.read_csv("inputs/matrix_" + engine + ".csv", sep=",")
+        ds = ds.reindex(sorted(ds.columns), axis=1)
+        ds = ds.sort_values(by=["asubsystem"])
+        map_dependencies[engine] = np.array(ds.values[0:99, 1:18])
 
-    sum2 = map_dependencies['cocos2d-x'] + map_dependencies['godot'] + map_dependencies['urho3d']
-    sns.set(rc={'figure.figsize':(12,8)})
+    sum = map_dependencies[engines[0]]
+    for i in range(1, len(engines)):
+        sum = np.add(sum, map_dependencies[engines[i]])
+
+    # do not annotate values 0 and 1
+    annot_values = np.array(sum, dtype=str)
+    annot_values = np.where(sum < 2, np.array([""], dtype="U1"), annot_values)
+
+    # set figure size
+    sns.set(rc={"figure.figsize": (12, 8)})
 
     # pallette chosen because it is good for colorblind
     colormap = sns.color_palette("bwr")
-    hm = sns.heatmap(sum2.tolist(), vmin=0, vmax=3, annot=True, 
-                    xticklabels=subsystems, yticklabels=subsystems, cmap=colormap, cbar=True)
+    hm = sns.heatmap(
+        sum.tolist(),
+        vmin=0,
+        vmax=9,
+        annot=annot_values,
+        xticklabels=subsystems,
+        yticklabels=subsystems,
+        cmap=colormap,
+        cbar=True,
+        fmt="",
+    )
     fig = hm.get_figure()
-    fig.savefig("heatmap.pdf") 
+    fig.savefig("outputs/heatmap.pdf")
+
+    # generate csv for column and row counts
+    sumRows(sum, subsystems)
+    sumCols(sum, subsystems)
+
 
 # get the sum for each column in the heatmap
 # useful to understand how much included a subsystem is
 def sumCols(matrix, subsystems):
     vals = []
-    for i in range(0,16):
-        vals.append(matrix[:,i].sum())
+    sub_count = len(subsystems)
+    for i in range(0, sub_count):
+        vals.append(matrix[:, i].sum())
     ds_most_inc_by = pd.DataFrame()
-    ds_most_inc_by['sub'] = subsystems
-    ds_most_inc_by['values'] = vals
-    result = ds_most_inc_by.sort_values(by=['values'], ascending=False)
-    print(result)
+    ds_most_inc_by["subsystem"] = subsystems
+    ds_most_inc_by["included_by"] = vals
+    result = ds_most_inc_by.sort_values(by=["included_by"], ascending=False)
+    result.to_csv("outputs/included_by.csv")
 
 # get the sum for each row in the heatmap
 # useful to understand how much a subsystem includes
@@ -44,9 +68,10 @@ def sumRows(matrix, subsystems):
         vals.append(line.sum())
         count += 1
     ds_most_inc = pd.DataFrame()
-    ds_most_inc['sub'] = subsystems
-    ds_most_inc['values'] = vals
-    result = ds_most_inc.sort_values(by=['values'], ascending=False)
-    print(result)
+    ds_most_inc["subsystem"] = subsystems
+    ds_most_inc["include"] = vals
+    result = ds_most_inc.sort_values(by=["include"], ascending=False)
+    result.to_csv("outputs/include.csv")
+
 
 main()
